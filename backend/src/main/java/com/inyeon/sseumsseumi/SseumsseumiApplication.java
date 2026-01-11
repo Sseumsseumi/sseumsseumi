2a163079e1b877a96905de70913ca85a926cbe73
Mun Hyerin <9993hye@naver.com>
2026-01-11 16:17:24 +0900

    [setting] : 백엔드 프로젝트 생성

---
+++ backend/src/main/java/com/inyeon/sseumsseumi/SseumsseumiApplication.java
@@ -0,0 +1,13 @@
+package com.inyeon.sseumsseumi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SseumsseumiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SseumsseumiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/inyeon/sseumsseumi/SseumsseumiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inyeon.sseumsseumi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SseumsseumiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
