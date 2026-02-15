02769ba87889de2e73a1f5cc8768d324b30200cd
Mun Hyerin <9993hye@naver.com>
2026-02-15 17:52:40 +0900

    [chore] : RedisCofig 클래스 수정

--- backend/src/main/java/com/inyeon/sseumsseumi/global/config/RedisConfig.java
+++ backend/src/main/java/com/inyeon/sseumsseumi/global/config/RedisConfig.java
@@ -1,4 +1,15 @@
 package com.inyeon.sseumsseumi.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories
 public class RedisConfig {
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
 }
