f6f6d3294c0371b9a20c154e7574534bbdb0f26e
nayeon <kknyapple@gmail.com>
2026-03-22 17:39:16 +0900

    [feat] : 거래목록 조회 api 연결

--- frontend/src/components/dashboard/transactions/TransactionTable.tsx
+++ frontend/src/components/dashboard/transactions/TransactionTable.tsx
@@ -1,7 +1,7 @@
-import type { Transaction } from "../../../types/transaction";
+import type { TransactionResponse } from "../../../api/transaction";
 
 interface Props {
-  transactions: Transaction[];
+  transactions: TransactionResponse[];
 }
 
 const TransactionTable = ({ transactions }: Props) => {
@@ -10,28 +10,41 @@ const TransactionTable = ({ transactions }: Props) => {
       <thead>
         <tr>
           <th>카테고리</th>
-          <th>계좌</th>
+          <th>지점</th>
           <th>거래일자</th>
+          <th>내용</th>
           <th>금액</th>
         </tr>
       </thead>
+
       <tbody>
-        {transactions.map((tx) => (
-          <tr key={tx.id}>
-            <td>{tx.category}</td>
-            <td>{tx.accountType}</td>
-            <td>{tx.date}</td>
-            <td
-              style={{
-                color: tx.type === "OUT" ? "red" : "blue",
-                textAlign: "right",
-              }}
-            >
-              {tx.type === "OUT" ? "-" : "+"}
-              {tx.amount.toLocaleString()}원
-            </td>
-          </tr>
-        ))}
+        {transactions.map((tx) => {
+          const isOut = tx.transactionWithdrawal > 0;
+          const amount = isOut
+            ? tx.transactionWithdrawal
+            : tx.transactionDeposit;
+
+          return (
+            <tr key={tx.transactionId}>
+              <td>{tx.categoryName}</td>
+              <td>{tx.transactionBranch}</td>
+              <td>
+                {tx.transactionDate} {tx.transactionTime}
+              </td>
+              <td>{tx.transactionContent}</td>
+
+              <td
+                style={{
+                  color: isOut ? "red" : "blue",
+                  textAlign: "right",
+                }}
+              >
+                {isOut ? "-" : "+"}
+                {amount.toLocaleString()}원
+              </td>
+            </tr>
+          );
+        })}
       </tbody>
     </table>
   );
