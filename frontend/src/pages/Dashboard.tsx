aff1bcadfaea080bd180c4ad2d1838867d9e170a
nayeon <kknyapple@gmail.com>
2026-03-22 18:04:17 +0900

    [fix] : 불필요한 파일 삭제

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -2,13 +2,12 @@ import Layout from "../components/layout/Layout";
 import DashboardLayout from "../components/layout/DashboardLayout";
 import AnalysisDashboard from "../components/dashboard/analysis/AnalysisDashboard.tsx";
 import StatisticsDashboard from "../components/dashboard/statics/StatisticsDashboard";
-import { transactions } from "../data/transactions";
 
 const Dashboard = () => {
   return (
     <Layout>
       <DashboardLayout
-        left={<StatisticsDashboard transactions={transactions} />}
+        left={<StatisticsDashboard />}
         right={<AnalysisDashboard />}
       />
     </Layout>
