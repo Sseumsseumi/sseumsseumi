437771b0c3eeb3e18226e86a94208c41939e36df
Mun Hyerin <9993hye@naver.com>
2026-03-28 20:07:27 +0900

    [feat] : 쿼리 카운터 구현

---
+++ backend/src/main/java/com/inyeon/sseumsseumi/global/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.inyeon.sseumsseumi.global.config;
+
+import com.inyeon.sseumsseumi.monitoring.config.QueryCountInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final QueryCountInterceptor queryCountInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(queryCountInterceptor).addPathPatterns("/**");
+    }
+}
---
+++ backend/src/main/java/com/inyeon/sseumsseumi/monitoring/config/QueryCountInspector.java
@@ -0,0 +1,17 @@
+package com.inyeon.sseumsseumi.monitoring.config;
+
+import org.hibernate.resource.jdbc.spi.StatementInspector;
+
+public class QueryCountInspector implements StatementInspector {
+    // 모든 SQL 실행마다 inspect() 메서드 자동으로 호출 (Hibernate 레벨)
+    @Override
+    public String inspect(String sql) {
+        // HTTP 요청 컨텍스트
+        RequestContext requestContext = RequestContextHolder.getContext();
+        if (requestContext != null) {
+            requestContext.incrementQueryCount(sql);
+        }
+
+        return sql;
+    }
+}
