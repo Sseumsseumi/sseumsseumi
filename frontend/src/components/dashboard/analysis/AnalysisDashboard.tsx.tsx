779108701aed6a5eb373b752ca5e4dd3ec976ad9
nayeon <kknyapple@gmail.com>
2026-03-29 17:17:20 +0900

    [refactor] : css 수정

--- frontend/src/components/dashboard/analysis/AnalysisDashboard.tsx.tsx
+++ frontend/src/components/dashboard/analysis/AnalysisDashboard.tsx.tsx
@@ -1,35 +1,25 @@
-import { useEffect, useState } from "react";
 import AuthSection from "../../user/AuthSection";
-import SummarySection from "./SummarySection";
+// import SummarySection from "./SummarySection";
 import AccountSection from "../../user/AccountSection";
 
 const LeftDashboard = () => {
-  const [isLogin, setIsLogin] = useState(false);
-
-  useEffect(() => {
-    const loginState = localStorage.getItem("isLogin");
-    setIsLogin(loginState === "true");
-  }, []);
-
   return (
     <section
       style={{
         background: "#ffffff",
-        padding: "30px",
+        padding: "10px",
         borderRadius: "12px",
         display: "flex",
         flexDirection: "column",
-        gap: "20px",
+        gap: "10px",
       }}
     >
-      <AuthSection />
-
-      {isLogin && (
-        <>
-          <SummarySection />
+       <>
+          <AuthSection />
+          {/* <SummarySection /> */}
           <AccountSection />
         </>
-      )}
+
     </section>
   );
 };
